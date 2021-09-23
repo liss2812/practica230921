8579bfaa704d2c3071e4a08de516284cc3dad1e7
¨Liss <lissramos2812@mail.com>
2021-09-23 11:57:46 -0500

    Agregar archivos de proyecto.

---
+++ practica230921/Documento.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica230921
+{
+    class Documento
+    {
+
+        //definir miembros
+        // propiedades 
+
+        private int _documentId;
+        private string _titulo;
+
+        private string _descripcion; 
+        private int _fechacreacion;
+    
+        //metodos de propiedades
+        // get-- obtiene el valor almacenado en la propiedad
+        // set-- asignar
+        
+
+        public void setDocumentId(int documentId)
+        {
+            _documentId = documentId;
+        }
+        public int getDocumentId()
+        {
+            return _documentId;
+        
+      }
+        //titulo
+        public void setTitulo(string titulo)
+        {
+            _titulo = titulo;
+        }
+        public string getTitulo()
+        {
+            return _titulo;
+        }
+
+      
+
+       
+    }
+}
---
+++ practica230921/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica230921
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // instancia de la clase documento 
+            Documento documento = new Documento();
+            documento.setDocumentId(1);
+            documento.setTitulo("practica");
+
+            MessageBox.Show(documento.getDocumentId() + documento.getTitulo());
+
+           
+        }
+    }
+}
